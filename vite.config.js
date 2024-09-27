e94870c1bc4c1c16424a9da9f747e966d6ea27e8
Abdelsalam Ebrahim <abdeslalamebrahim21@gmail.com>
2024-09-27 15:29:33 +0300

    Add Files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Realtime-Chatapp/",
   plugins: [react()],
-  base: "/vite-react-deploy/"
 })
